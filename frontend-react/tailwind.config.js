b8e5fecb7fd83780e47940c7c57cfee942a32c46
Gabriel Rodrigues <jipacoding@gmail.com>
2021-06-01 06:16:37 -0400

    feat(frontend-react): setup tailwind and reset app

---
+++ frontend-react/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  prefix: "tw-",
+  darkMode: false, // or 'media' or 'class'
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
