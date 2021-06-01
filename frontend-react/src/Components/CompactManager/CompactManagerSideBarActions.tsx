533af68e873ce7efdeac5c39d1bfad7ed9e66811
Gabriel Rodrigues <jipacoding@gmail.com>
2021-06-01 15:00:35 -0400

    feat(frontend-react): CompactManager: SideBarActions: ActionsImportFiles

--- frontend-react/src/Components/CompactManager/CompactManagerSideBarActions.tsx
+++ frontend-react/src/Components/CompactManager/CompactManagerSideBarActions.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { ActionsImportFiles } from "./SideBarActions";
 
 function CompactManagerSideBarActions() {
   return (
     <>
+      <ActionsImportFiles />
     </>
   );
 }
