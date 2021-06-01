d3dd3531466c28d7e9af8f3bb366c093567eadcd
Gabriel Rodrigues <jipacoding@gmail.com>
2021-06-01 15:04:31 -0400

    feat(frontend-react): useArchive (libarchive.js)

---
+++ frontend-react/src/Components/Hooks/useArchive.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable import/no-webpack-loader-syntax */
+import { useRef } from "react";
+import { Archive } from "libarchive.js/main.js";
+
+Archive.init({ workerUrl: import.meta.env.BASE_URL + "worker-bundle.js" });
+
+export const useArchive = () => {
+  const { current } = useRef(Archive);
+  return current;
+};
