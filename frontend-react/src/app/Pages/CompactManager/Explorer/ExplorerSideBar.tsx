9a14551e1136cdbac3dce6c4a365faed0cbeb1af
Gabriel Rodrigues <jipacoding@gmail.com>
2021-06-02 22:25:48 -0400

    feat(frontend-react): CompactManager: CompactExplorerSideBarActions

--- frontend-react/src/app/Pages/CompactManager/Explorer/ExplorerSideBar.tsx
+++ frontend-react/src/app/Pages/CompactManager/Explorer/ExplorerSideBar.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Divider from "@material-ui/core/Divider";
+import CompactExplorerSideBarActions from "./ExplorerSideBarActions";
 import { StyledCompactExplorerSideBar } from "./styles";
 
 export default function CompactExplorerSideBar() {
   return (
     <StyledCompactExplorerSideBar>
+      <CompactExplorerSideBarActions />
+      <Divider />
     </StyledCompactExplorerSideBar>
   );
 }
---
+++ frontend-react/src/app/Pages/CompactManager/Explorer/ExplorerSideBarActions.tsx
@@ -0,0 +1,9 @@
+import Divider from "@material-ui/core/Divider";
+import React from "react";
+
+export default function CompactExplorerSideBarActions() {
+  return (
+    <>
+    </>
+  );
+}
